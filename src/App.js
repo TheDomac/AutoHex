0f42ea253a4d70e9e7085cdfc81fb133055d02f9
Domagoj Zganec <domagoj.zganec1@gmail.com>
2019-09-19 00:07:55 +0200

    writing steps function - start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { reduce } from "lodash"
 import logo from './logo.svg';
 import './App.css';
 
+import fields from "./common/consts/fields"
+
+function steps(startFieldId, endFieldId) {
+
+  let count = 1
+  let paths = []
+  let idsToCheck = fields[startFieldId].neighborFieldsIds
+  while (paths.length === 0) {
+
+    for(var i = 0; i < count; i ++ ) {
+      if (idsToCheck.includes(endFieldId)) {
+        console.log("found", paths)
+        paths = ["weeeee"]
+      } else {
+        count ++;
+        idsToCheck = idsToCheck.reduce((prev, idToCheck) => {
+          console.log("prev", prev)
+          const idsWithoutPreviouslyChecked = fields[idToCheck].neighborFieldsIds.filter(id => !prev.includes(id))
+          return idsWithoutPreviouslyChecked
+        }, [])
+        console.log("-----------------")
+      }
+    }
+  }
+}
+
+
 function App() {
+  console.log(steps(1, 21))
   return (
     <div className="App">
       <header className="App-header">
