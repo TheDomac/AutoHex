d84509c228538cb3337354cb6545eee6772c55e9
Domagoj Zganec <domagoj.zganec1@gmail.com>
2019-09-19 21:38:55 +0200

    cleared errors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import getPathsBetweenTwoPoints from "common/utils/getPathsBetweenTwoPoints";
 
 function App() {
-  let a = "kurac";
-
   console.log(getPathsBetweenTwoPoints(1, 21));
   return <div />;
 }
