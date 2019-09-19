87fc3f99c01a788d2afeb9a6506846e24e210706
Domagoj Zganec <domagoj.zganec1@gmail.com>
2019-09-19 21:07:00 +0200

    test error

--- src/common/utils/setupBoard.js
+++ src/common/utils/setupBoard.js
@@ -1,5 +1,8 @@
 import fields from "../consts/fields";
 
-const setupBoard = (player1, player2) => {};
+const setupBoard = (player1, player2) => {
+  const a = 1;
+  a = 2;
+};
 
 export default setupBoard;
