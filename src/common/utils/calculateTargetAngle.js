b99f2502969b928450200220cb0c7d0a6c730c82
Domagoj Žganec <domagoj.zganec1@gmail.com>
2019-10-15 14:04:59 +0200

    calculate target angle 1 - needs path

--- src/common/utils/calculateTargetAngle.js
+++ src/common/utils/calculateTargetAngle.js
@@ -1,8 +1,18 @@
 import slots from "common/consts/slots";
 
-const calculateTargetAngle = (mySlotid, targetSlotId) => {
-  const index = slots[mySlotid].adjacentSlotsIds.findIndex(slotId => slotId === targetSlotId);
+const getAngleBetweenAdjecentSlots = (mySlotId, targetSlotId) => {
+  const index = slots[mySlotId].adjacentSlotsIds.findIndex(slotId => slotId === targetSlotId);
+
   return 30 + index * 60;
 };
+
+const calculateTargetAngle = (mySlotId, targetSlotId, path) =>
+  path.reduce((prev, slot, i) => {
+    if (!path[i + 1]) {
+      return prev + getAngleBetweenAdjecentSlots(slot, targetSlotId) / i;
+    }
+
+    return prev + getAngleBetweenAdjecentSlots(slot, path[i + 1]) / i;
+  }, calculateTargetAngle(mySlotId, path[0] || targetSlotId));
 
 export default calculateTargetAngle;
